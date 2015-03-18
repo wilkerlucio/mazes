69672fa45b952667b310143638c02c2206d04bc4
Wilker Lucio <wilkerlucio@gmail.com>
2015-03-17 21:40:44 -0300

    auto draw maze on figwheel reload

--- src/dev/mazes/dev.cljs
+++ src/dev/mazes/dev.cljs
@@ -1,9 +1,10 @@
 (ns mazes.dev
   (:require [figwheel.client :as fw]
+            [mazes.core]
             [mazes.core-test]
             [cemerick.cljs.test :as t]))
 
 (fw/start {
   :websocket-url "ws://localhost:3449/figwheel-ws"
   :on-jsload (fn []
-               )})
+               (mazes.core/sample-canvas-draw 10))})

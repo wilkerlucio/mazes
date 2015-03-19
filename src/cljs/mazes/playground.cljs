df4334bfc9359610b7b405db8e9edf5aa9a963ef
Wilker Lucio <wilkerlucio@gmail.com>
2015-03-19 12:27:00 -0300

    rendering with SVG and playground cleanup

--- src/cljs/mazes/playground.cljs
+++ src/cljs/mazes/playground.cljs
@@ -7,63 +7,13 @@
             [om.dom :as dom]
             [cljs.core.async :refer [chan put!] :as async]))
 
-(defn canvas-line [ctx [x1 y1] [x2 y2]]
-  (doto ctx
-    (.beginPath)
-    (.moveTo x1 y1) (.lineTo x2 y2)
-    (.stroke)))
+;; state and data
 
-(defn cell-bounds [[y x] cell-size]
-  [(* x cell-size) (* y cell-size)
-   (* (inc x) cell-size) (* (inc y) cell-size)])
-
-(defn draw-grid [ctx grid cell-size]
-  (bench "draw grid"
-    (doseq [cell (cells-seq grid) :let [[x1 y1 x2 y2] (cell-bounds cell cell-size)]]
-      (if-not (valid-pos? grid (north cell)) (canvas-line ctx [x1 y1] [x2 y1]))
-      (if-not (valid-pos? grid (west cell)) (canvas-line ctx [x1 y1] [x1 y2]))
-      (if-not (linked-to? grid cell (east cell)) (canvas-line ctx [x2 y1] [x2 y2]))
-      (if-not (linked-to? grid cell (south cell)) (canvas-line ctx [x1 y2] [x2 y2])))))
-
-(defn color-compute-blue-to-red [x]
-  (let [color (->> (* 255 x)
-                   (.round js/Math)
-                   (- 255))]
-    (str "rgb(" (- 255 color) ", " color ", " color ")")))
-
-(defn color-compute-green-to-black [x]
-  (let [color (->> (* 255 x)
-                   (.round js/Math)
-                   (- 255))]
-    (str "rgb(" 0 ", " color ", " 0 ")")))
-
-(defn draw-backgrounds [ctx marks cell-size color-fn]
-  (bench "draw background"
-    (let [max-distance (get marks (farthest-point marks))]
-      (doseq [[cell distance] marks
-              :let [[x y] (cell-bounds cell cell-size)]]
-        (doto ctx
-          (aset "fillStyle" (color-fn (/ distance max-distance)))
-          (.fillRect x y cell-size cell-size))))))
-
-;; misc
-
-(defn draw-maze [canvas
-                 {:keys [grid-size gen-algorithm mark-builder color-fn]
-                  :or   {grid-size     10
-                         gen-algorithm m/gen-binary-tree
-                         mark-builder  #(dijkstra-enumerate % (rand-cell %))
-                         color-fn      color-compute-blue-to-red}}]
-  (let [ctx (.getContext canvas "2d")
-        grid (bench "generating maze" (-> (make-grid grid-size grid-size) gen-algorithm))
-        cell-size (/ (.-width canvas) (:columns grid))
-        marks (mark-builder grid)]
-    (doto ctx
-      (.clearRect 0 0 (.-width canvas) (.-height canvas))
-      (.save)
-      (draw-backgrounds marks cell-size color-fn)
-      (draw-grid grid cell-size)
-      (.restore))))
+(defonce app-state
+  (atom {:grid-size      10
+         :generator      :binary-tree
+         :marker-builder :random-point
+         :colorizer      :blue-to-red}))
 
 (def opt-algorithms
   (sorted-map
@@ -80,11 +30,40 @@
     :wilson {:label "Wilson's"
              :value m/gen-wilson}))
 
+;; helpers
+
+(defn cell-bounds [[y x] cell-size]
+  [(* x cell-size) (* y cell-size)
+   (* (inc x) cell-size) (* (inc y) cell-size)])
+
 (defn impl->options [m]
   (map (fn [[k v]] {:label (:label v) :value (name k)}) m))
 
 (defn attrs [base & removals]
   (clj->js (apply dissoc base removals)))
+
+(defn fit-in-range [n mi ma] (max (min n ma) mi))
+
+;; color functions
+
+(defn color-compute-blue-to-red [x]
+  (let [color (->> (* 255 x)
+                   (.round js/Math)
+                   (- 255))]
+    (str "rgb(" (- 255 color) ", " color ", " color ")")))
+
+(defn color-compute-green-to-black [x]
+  (let [color (->> (* 255 x)
+                   (.round js/Math)
+                   (- 255))]
+    (str "rgb(" 0 ", " color ", " 0 ")")))
+
+;; svg helpers
+
+(defn svg-line [x1 y1 x2 y2]
+  (dom/line #js {:x1 x1 :y1 y1 :x2 x2 :y2 y2 :style #js {:stroke "#000" :stroke-width "1"}}))
+
+;; components
 
 (defn comp-option [{:keys [label value]}] (dom/option #js {:value value} label))
 
@@ -92,54 +71,33 @@
   (apply dom/select (attrs attributes :options)
          (map comp-option options)))
 
-(defn comp-grid [{:keys [grid marks] :as attributes} owner]
-  (let [render (fn []
-                 (let [canvas (om/get-node owner "canvas")
-                       ctx (.getContext canvas "2d")
-                       cache-backgrounds (om/get-state owner :cache-backgrounds)
-                       cache-grid (om/get-state owner :cache-grid)]
-                   (bench "rendering from cache"
-                     (.clearRect ctx 0 0 (.-width canvas) (.-height canvas))
-                     (if cache-backgrounds (.drawImage ctx cache-backgrounds 0 0))
-                     (if cache-grid (.drawImage ctx cache-grid 0 0)))))
-        cache-render (fn [name render-fn]
-                       (let [canvas (doto (.createElement js/document "canvas")
-                                      (aset "width" 600) (aset "height" 600))
-                             ctx (.getContext canvas "2d")
-                             cell-size (/ (.-width canvas) (:columns grid))]
-                         (render-fn ctx cell-size)
-                         (om/set-state! owner name canvas)))
-        render-backgrounds (fn []
-                             (cache-render :cache-backgrounds
-                               #(draw-backgrounds % marks %2 color-compute-blue-to-red)))
-        render-grid (fn [] (cache-render :cache-grid #(draw-grid % grid %2)))]
-    (reify
-      om/IDidMount
-      (did-mount [_]
-        (render-backgrounds)
-        (render-grid)
-        (render))
-      om/IDidUpdate
-      (did-update [_ pp _]
-        (if (not= (:grid pp) grid) (render-grid))
-        (if (or (not= (:marks pp) marks)
-                (not= (:marks pp) marks)) (render-backgrounds))
-        (render))
-      om/IRender
-      (render [_]
-        (dom/canvas (-> (assoc attributes :ref "canvas")
-                        (attrs :grid)))))))
+(defn comp-grid-backgrounds [{:keys [width color-fn]
+                         :or {color-fn color-compute-blue-to-red}}
+                        {:keys [marks columns]}]
+  (let [max-distance (get marks (farthest-point marks))
+        cell-size (/ width columns)
+        mark->rect (fn [[cell distance]]
+                     (let [[x y] (cell-bounds cell cell-size)]
+                       (dom/rect #js {:width cell-size :height cell-size :x x :y y
+                                      :style #js {:fill (color-fn (/ distance max-distance))}})))]
+    (apply dom/g nil (map mark->rect marks))))
 
-(defonce app-state
-  (atom {:grid-size      10
-         :generator      :binary-tree
-         :marker-builder :random-point
-         :colorizer      :blue-to-red}))
+(defn comp-grid-lines [{:keys [width]} {:keys [columns] :as grid}]
+  (let [cell-size (/ width columns)
+        link->line (fn [cell]
+                     (let [[x1 y1 x2 y2] (cell-bounds cell cell-size)
+                           lines (->> [(if-not (valid-pos? grid (north cell)) [x1 y1 x2 y1])
+                                       (if-not (valid-pos? grid (west cell)) [x1 y1 x1 y2])
+                                       (if-not (linked-to? grid cell (east cell)) [x2 y1 x2 y2])
+                                       (if-not (linked-to? grid cell (south cell)) [x1 y2 x2 y2])]
 
-(defn fit-in-range [n mi ma] (max (min n ma) mi))
+                                      (filter identity))]
+                       (apply dom/g #js {:key (pr-str cell)} (map #(apply svg-line %) lines))))]
+    (apply dom/g nil (map link->line (cells-seq grid)))))
 
 (defn maze-playground [{:keys [generator grid-size] :as data} owner]
   (reify
+    om/IDisplayName (display-name [_] "Maze Playground")
     om/IInitState
     (init-state [_]
       (let [bus (chan 1024)
@@ -164,8 +122,7 @@
                 generator (get-in opt-algorithms [(:generator @app-state) :value])
                 maze (bench "generating maze" (-> (m/make-grid grid-size grid-size) generator))
                 marks (bench "generating marks" (-> (m/dijkstra-enumerate maze (m/rand-cell maze))))]
-            (om/update! data :maze maze)
-            (om/update! data :marks marks)))))
+            (om/update! data :maze (assoc maze :marks marks))))))
 
     om/IRender
     (render [_]
@@ -182,9 +139,13 @@
           (dom/button #js {:onClick #(put! bus [:generate-maze])
                            :style #js {:margin-top "10px"}} "Generate maze")
           (dom/hr nil)
-          (om/build comp-grid {:grid (:maze data) :marks (:marks data)
-                               :width 600 :height 600
-                               :style #js {:border "1px solid #000"}}))))))
+
+          (let [size {:width 600 :height 600}]
+            (dom/svg (clj->js size)
+              (comp-grid-backgrounds size (:maze data))
+              (comp-grid-lines size (:maze data)))))))))
+
+;; initializer
 
 (defn build-at [node]
   (let [root (om/root maze-playground app-state {:target node})]
